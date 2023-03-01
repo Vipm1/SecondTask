981ca9e4435aa648e71211cdd32556a14830f6de
Vipm1 <80585715+Vipm1@users.noreply.github.com>
2023-03-01 14:16:24 +0300

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5a2e13416032a00d008c45ff43ae0b7252d299ae',
+        'name' => 'danil/vipm1',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'danil/vipm1' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5a2e13416032a00d008c45ff43ae0b7252d299ae',
+            'dev_requirement' => false,
+        ),
+    ),
+);
